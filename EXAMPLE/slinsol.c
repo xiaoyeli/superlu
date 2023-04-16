c3ddb30f50433e9be8d7db1f6c9b7d8cf8bdcc44
Christoph Grüninger <foss@grueninger.de>
2023-04-16 15:13:31 +0200

    [example] Add Doxygen description to slinsol

--- EXAMPLE/slinsol.c
+++ EXAMPLE/slinsol.c
@@ -1,4 +1,4 @@
-/*! \file
+/*
 Copyright (c) 2003, The Regents of the University of California, through
 Lawrence Berkeley National Laboratory (subject to receipt of any required 
 approvals from U.S. Dept. of Energy) 
@@ -15,6 +15,10 @@ at the top-level directory.
  * and Lawrence Berkeley National Lab.
  * October 15, 2003
  *
+ */
+
+/*! \file
+ * Example: use simple driver DGSSV to solve a linear system one time
  */
 #include <stdio.h>
 #include <stdlib.h>

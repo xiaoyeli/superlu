f5f29f8ae8a34e89352a12e2fe832394602bd8a7
Christoph Grüninger <foss@grueninger.de>
2023-04-24 19:45:03 +0200

    [doxygen] Improve documentation for remaining testing files
    Fix use of \file to not include just the license text.
    Add \param and remove non-doxygen formattings.
    Use Doxygen module

--- TESTING/sp_ienv.c
+++ TESTING/sp_ienv.c
@@ -1,4 +1,4 @@
-/*! \file
+/*
 Copyright (c) 2003, The Regents of the University of California, through
 Lawrence Berkeley National Laboratory (subject to receipt of any required 
 approvals from U.S. Dept. of Energy) 
@@ -8,52 +8,49 @@ All rights reserved.
 The source code is distributed under BSD license, see the file License.txt
 at the top-level directory.
 */
-/*
- * File name:		sp_ienv.c
- * History:             Modified from lapack routine ILAENV
+
+/*! \file
+ * Choose machine-dependent parameters for the
+ * local environment.
+ *
+ * History: Modified from lapack routine ILAENV
+ *
+ * \ingroup Testing
  */
+
 #include "slu_Cnames.h"
 
+/*!
+ * sp_ienv() is inquired to choose machine-dependent parameters for the
+ * local environment.
+ *
+ * See ISPEC for a description of the parameters.
+ *
+ * This version provides a set of parameters which should give good,
+ * but not optimal, performance on many of the currently available
+ * computers.  Users are encouraged to modify this subroutine to set
+ * the tuning parameters for their particular machine using the option
+ * and problem size information in the arguments.
+ *
+ * \param[in] ispec   Specifies the parameter to be returned as the value of SP_IENV.<br/>
+ *         = 1: the panel size w; a panel consists of w consecutive
+ *         columns of matrix A in the process of Gaussian elimination.
+ *         The best value depends on machine's cache characters.<br/>
+ *         = 2: the relaxation parameter relax; if the number of
+ *         nodes (columns) in a subtree of the elimination tree is less
+ *         than relax, this subtree is considered as one supernode,
+ *         regardless of the their row structures.<br/>
+ *         = 3: the maximum size for a supernode;<br/>
+ *         = 4: the minimum row dimension for 2-D blocking to be used;<br/>
+ *         = 5: the minimum column dimension for 2-D blocking to be used;<br/>
+ *         = 6: the estimated fills factor for L and U, compared with A;
+ *
+ * \result >= 0: the value of the parameter specified by ISPEC<br/>
+ *         < 0:  if SP_IENV = -k, the k-th argument had an illegal value.
+ */
 int
 sp_ienv(int ispec)
 {
-/*
-    Purpose   
-    =======   
-
-    sp_ienv() is inquired to choose machine-dependent parameters for the
-    local environment. See ISPEC for a description of the parameters.   
-
-    This version provides a set of parameters which should give good,   
-    but not optimal, performance on many of the currently available   
-    computers.  Users are encouraged to modify this subroutine to set   
-    the tuning parameters for their particular machine using the option   
-    and problem size information in the arguments.   
-
-    Arguments   
-    =========   
-
-    ISPEC   (input) int
-            Specifies the parameter to be returned as the value of SP_IENV.   
-            = 1: the panel size w; a panel consists of w consecutive
-	         columns of matrix A in the process of Gaussian elimination.
-		 The best value depends on machine's cache characters.
-            = 2: the relaxation parameter relax; if the number of
-	         nodes (columns) in a subtree of the elimination tree is less
-		 than relax, this subtree is considered as one supernode,
-		 regardless of the their row structures.
-            = 3: the maximum size for a supernode;
-	    = 4: the minimum row dimension for 2-D blocking to be used;
-	    = 5: the minimum column dimension for 2-D blocking to be used;
-	    = 6: the estimated fills factor for L and U, compared with A;
-	    
-   (SP_IENV) (output) int
-            >= 0: the value of the parameter specified by ISPEC   
-            < 0:  if SP_IENV = -k, the k-th argument had an illegal value. 
-  
-    ===================================================================== 
-*/
-
     switch (ispec) {
 	case 1: return (3);
 	case 2: return (2);

358d3d04ef4b8e337af8724b316b2836b3ed4cb6
Christoph Grüninger <foss@grueninger.de>
2023-05-06 21:25:02 +0200

    [matgen] Remove unused typedefs from f2c.h

--- TESTING/MATGEN/f2c.h
+++ TESTING/MATGEN/f2c.h
@@ -9,27 +9,12 @@
 #ifndef F2C_INCLUDE
 #define F2C_INCLUDE
 
-#if 0
-typedef long int integer; /* 64 on 64-bit machine */
-#endif
-
 typedef int integer;
 
-typedef char *address;
-typedef short int shortint;
 typedef float real;
 typedef double doublereal;
 typedef struct { real r, i; } complex;
 typedef struct { doublereal r, i; } doublecomplex;
-typedef short int shortlogical;
-typedef char logical1;
-typedef char integer1;
-/* typedef long long longint; */ /* system-dependent */
-
-/* Extern is for use with -E */
-#ifndef Extern
-#define Extern extern
-#endif
 
 #define min(a,b) ((a) <= (b) ? (a) : (b))
 #define max(a,b) ((a) >= (b) ? (a) : (b))
